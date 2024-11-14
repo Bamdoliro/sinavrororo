6f7e3448a8dbd04552c78141e7af2f938ebde132
oxjadex <oharin765@gmail.com>
2024-11-14 23:59:00 +0900

    feat: 아이콘 추가

---
+++ public/svgs/Profile.tsx
@@ -0,0 +1,27 @@
+import type { SVGProps } from "react";
+import React from "react";
+
+const IconHomeBlack = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      width="52"
+      height="52"
+      viewBox="0 0 52 52"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <rect width="52" height="52" rx="26" fill="#FFBA99" />
+      <path
+        d="M26.0002 26C29.6821 26 32.6668 23.0153 32.6668 19.3334C32.6668 15.6515 29.6821 12.6667 26.0002 12.6667C22.3183 12.6667 19.3335 15.6515 19.3335 19.3334C19.3335 23.0153 22.3183 26 26.0002 26Z"
+        fill="#FF7433"
+      />
+      <path
+        d="M25.9999 29.3333C19.3199 29.3333 13.8799 33.8133 13.8799 39.3333C13.8799 39.7066 14.1732 40 14.5465 40H37.4532C37.8266 40 38.1199 39.7066 38.1199 39.3333C38.1199 33.8133 32.6799 29.3333 25.9999 29.3333Z"
+        fill="#FF7433"
+      />
+    </svg>
+  );
+};
+
+export default IconHomeBlack;
---
+++ public/svgs/index.ts
@@ -0,0 +1,12 @@
+export { default as IconHomeBlack } from "./IconHomeBlack";
+export { default as IconHomeWhite } from "./IconHomeWhite";
+export { default as IconBellBlack } from "./IconBellBlack";
+export { default as IconBellWhite } from "./IconBellWhite";
+export { default as IconSupportBlack } from "./IconSupportBlack";
+export { default as IconSupportWhite } from "./IconSupportWhite";
+export { default as IconPhoneBlack } from "./IconPhoneBlack";
+export { default as IconPhoneWhite } from "./IconPhoneWhite";
+export { default as IconDelete } from "./IconDelete";
+export { default as IconAdd } from "./IconAdd";
+export { default as IconSearch } from "./IconSearch";
+export { default as IconCheck } from "./IconCheck";
