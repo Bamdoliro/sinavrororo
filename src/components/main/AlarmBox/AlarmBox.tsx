c04a6ebde00899142a9da7a494cb43dba866c91a
oxjadex <oharin765@gmail.com>
2024-11-27 10:18:54 +0900

    style: 기능 삭제 디자인 수정

--- src/components/main/AlarmBox/AlarmBox.tsx
+++ src/components/main/AlarmBox/AlarmBox.tsx
@@ -1,4 +1,4 @@
-import { color, font } from "@/styles";
+import { color } from "@/styles";
 import { Button, Column, Row, Text } from "@/ui/index";
 import { flex } from "@/utils";
 import styled from "styled-components";
@@ -6,35 +6,24 @@ import { useRouter } from "next/navigation";
 import { ROUTES } from "@/constants/common/constants";
 
 const AlarmBox = () => {
-  const AlarmCount = 2;
   const router = useRouter();
-
-  const TIME_DATA = ["12:00", "22:00"];
 
   return (
     <StyledApplicationBox onClick={() => router.push(ROUTES.ALARM)}>
-      <Column>
-        <Row justifyContent="space-between">
+      <Column gap={12}>
+        <Row justifyContent="flex-start">
           <Text fontType="Heading2" color={color.gray900}>
-            설정한 알림
+            알림 설정
           </Text>
         </Row>
-        <Text fontType="B2" color={color.gray600}>
-          오늘 푸시될 알람 {AlarmCount}개
-        </Text>
+        <Button
+          size="LARGE"
+          styleType="TERTIARY"
+          style={{ whiteSpace: "nowrap" }}
+        >
+          알림 설정으로 이동
+        </Button>
       </Column>
-      <Row gap={4}>
-        {TIME_DATA.map((time, index) => (
-          <TimeBox key={`time-data ${index}`}>{time}</TimeBox>
-        ))}
-      </Row>
-      <Button
-        size="LARGE"
-        styleType="TERTIARY"
-        style={{ whiteSpace: "nowrap" }}
-      >
-        알림 설정으로 이동
-      </Button>
     </StyledApplicationBox>
   );
 };
@@ -44,18 +33,10 @@ export default AlarmBox;
 const StyledApplicationBox = styled.div`
   ${flex({ flexDirection: "column", justifyContent: "space-between" })}
   width: 480px;
-  height: 248px;
+  height: 180px;
   padding: 28px 32px;
   border: 1px solid ${color.gray200};
   border-radius: 12px;
   gap: 16px;
   cursor: pointer;
-`;
-
-const TimeBox = styled.div`
-  ${font.Heading5}
-  border: 1px solid ${color.adminGray400};
-  border-radius: 999px;
-  padding: 6px 12px;
-  color: ${color.gray600};
 `;
